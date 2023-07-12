01dbb79e0a41003de7fff8cc5892eb7e33436221
Amy Haywood Dutton <me@amyhaywood.com>
2023-07-12 15:20:50 -0500

    ✨ Added script to remove Create Redwood App and run node script after the initial run

--- gitpod-setup.js
+++ gitpod-setup.js
@@ -22,3 +22,29 @@ fs.readFile('api/db/schema.prisma', 'utf8', (err, data) => {
     console.log('schema.prisma file updated successfully!')
   })
 })
+
+fs.readFile('.gitpod.yml', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+
+  // Replace the Create Redwood Command
+  const updatedContent = data.replace(
+    'yarn create redwood-app . --overwrite --yarn-install --typescript --git-init=false',
+    ''
+  ) // Replace the script that runs the node script
+  .replace(
+    'node gitpod-setup.js',
+    ''
+  )
+
+  // Write the modified content back to the file
+  fs.writeFile('.gitpod.yml', updatedContent, 'utf8', (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log('.gitpod.yml file updated successfully!')
+  })
+}
